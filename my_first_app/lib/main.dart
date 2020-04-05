8f58d77a1868b938ca1e43d3339de7f2615a0dfc
your.user.name <helgist18@ru.is>
2020-04-05 17:14:58 +0000

    first commit

---
+++ my_first_app/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: PeopleCart()
+));
+
+class PeopleCart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[600],
+      appBar: AppBar(
+        title: Text('Peoples cart'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(backgroundImage: AssetImage('assets/helgi_flutter.png'),
+              radius: 40.0,
+              ),
+            ),
+            Divider(height: 90.0,
+            color: Colors.grey[900],
+            ),
+          Text(
+            'NAME',
+            style: TextStyle(
+            color: Colors.grey,
+            letterSpacing: 2.0,
+            )
+          ),
+          
+          Text(
+            'Helgi Saevar',
+            style: TextStyle(
+            color: Colors.amber[200],
+            letterSpacing: 2.0,
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+            )
+          ),
+          SizedBox(height: 30.0),
+          Text(
+            'JOB POSITION',
+            style: TextStyle(
+            color: Colors.grey,
+            letterSpacing: 2.0,
+            )
+          ),
+          
+          Text(
+            'unemployed',
+            style: TextStyle(
+            color: Colors.amber[200],
+            letterSpacing: 2.0,
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+            )
+          ),
+          SizedBox(height: 30.0),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.email,
+                color: Colors.grey[400],
+              ),
+              SizedBox(width: 10.0),
+              Text('helgiisaevar@gmail.com',
+              style: TextStyle(
+              color: Colors.grey[400],
+              letterSpacing: 2.0,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              )),
+            ],
+          ),
+        ],),
+      )
+    );
+  }
+}
